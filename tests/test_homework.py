99464fc320ed232d906ff915c888f6af77033e9c
emmejiaa <emmejiaa@unal.edu.co>
2025-11-14 17:27:35 -0500

    test 01

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,6 +1,4 @@
 # flake8: noqa: E501
-"""Autograding script."""
-
 import gzip
 import json
 import os
@@ -9,18 +7,18 @@ import pickle
 import pandas as pd  # type: ignore
 
 # ------------------------------------------------------------------------------
-MODEL_FILENAME = "files/models/model.pkl.gz"
-MODEL_COMPONENTS = [
+MODEL_PATH = "files/models/model.pkl.gz"
+REQUIRED_COMPONENTS = [
     "OneHotEncoder",
     "SelectKBest",
     "MinMaxScaler",
     "LinearRegression",
 ]
-SCORES = [
+SCORE_LIMITS = [
     -1.590,
     -2.429,
 ]
-METRICS = [
+METRIC_LIMITS = [
     {
         "type": "metrics",
         "dataset": "train",
@@ -42,72 +40,66 @@ METRICS = [
 #
 # Internal tests
 #
-def _load_model():
-    """Generic test to load a model"""
-    assert os.path.exists(MODEL_FILENAME)
-    with gzip.open(MODEL_FILENAME, "rb") as file:
-        model = pickle.load(file)
-    assert model is not None
-    return model
+def _cargar_modelo():
+    assert os.path.exists(MODEL_PATH)
+    with gzip.open(MODEL_PATH, "rb") as archivo_modelo:
+        modelo = pickle.load(archivo_modelo)
+    assert modelo is not None
+    return modelo
 
 
-def _test_components(model):
-    """Test components"""
-    assert "GridSearchCV" in str(type(model))
-    current_components = [str(model.estimator[i]) for i in range(len(model.estimator))]
-    for component in MODEL_COMPONENTS:
-        assert any(component in x for x in current_components)
+def _verificar_componentes(modelo):
+    assert "GridSearchCV" in str(type(modelo))
+    pasos_actuales = [str(modelo.estimator[i]) for i in range(len(modelo.estimator))]
+    for componente in REQUIRED_COMPONENTS:
+        assert any(componente in paso for paso in pasos_actuales)
 
 
-def _load_grading_data():
-    """Load grading data"""
-    with open("files/grading/x_train.pkl", "rb") as file:
-        x_train = pickle.load(file)
+def _cargar_datos_calificacion():
+    with open("files/grading/x_train.pkl", "rb") as archivo:
+        x_train = pickle.load(archivo)
 
-    with open("files/grading/y_train.pkl", "rb") as file:
-        y_train = pickle.load(file)
+    with open("files/grading/y_train.pkl", "rb") as archivo:
+        y_train = pickle.load(archivo)
 
-    with open("files/grading/x_test.pkl", "rb") as file:
-        x_test = pickle.load(file)
+    with open("files/grading/x_test.pkl", "rb") as archivo:
+        x_test = pickle.load(archivo)
 
-    with open("files/grading/y_test.pkl", "rb") as file:
-        y_test = pickle.load(file)
+    with open("files/grading/y_test.pkl", "rb") as archivo:
+        y_test = pickle.load(archivo)
 
     return x_train, y_train, x_test, y_test
 
 
-def _test_scores(model, x_train, y_train, x_test, y_test):
-    """Test scores"""
-    assert model.score(x_train, y_train) < SCORES[0]
-    assert model.score(x_test, y_test) < SCORES[1]
+def _probar_scores(modelo, x_train, y_train, x_test, y_test):
+    assert modelo.score(x_train, y_train) < SCORE_LIMITS[0]
+    assert modelo.score(x_test, y_test) < SCORE_LIMITS[1]
 
 
-def _load_metrics():
+def _leer_metricas_archivo():
     assert os.path.exists("files/output/metrics.json")
-    metrics = []
-    with open("files/output/metrics.json", "r", encoding="utf-8") as file:
-        for line in file:
-            metrics.append(json.loads(line))
-    return metrics
+    lista_metricas: list[dict] = []
+    with open("files/output/metrics.json", "r", encoding="utf-8") as archivo:
+        for linea in archivo:
+            lista_metricas.append(json.loads(linea))
+    return lista_metricas
 
 
-def _test_metrics(metrics):
-
-    for index in [0, 1]:
-        assert metrics[index]["type"] == METRICS[index]["type"]
-        assert metrics[index]["dataset"] == METRICS[index]["dataset"]
-        assert metrics[index]["r2"] > METRICS[index]["r2"]
-        assert metrics[index]["mse"] < METRICS[index]["mse"]
-        assert metrics[index]["mad"] < METRICS[index]["mad"]
+def _comprobar_metricas(metricas):
+    for indice in [0, 1]:
+        assert metricas[indice]["type"] == METRIC_LIMITS[indice]["type"]
+        assert metricas[indice]["dataset"] == METRIC_LIMITS[indice]["dataset"]
+        assert metricas[indice]["r2"] > METRIC_LIMITS[indice]["r2"]
+        assert metricas[indice]["mse"] < METRIC_LIMITS[indice]["mse"]
+        assert metricas[indice]["mad"] < METRIC_LIMITS[indice]["mad"]
 
 
 def test_homework():
-    """Tests"""
 
-    model = _load_model()
-    x_train, y_train, x_test, y_test = _load_grading_data()
-    metrics = _load_metrics()
+    modelo_entrenado = _cargar_modelo()
+    x_train, y_train, x_test, y_test = _cargar_datos_calificacion()
+    metricas_generadas = _leer_metricas_archivo()
 
-    _test_components(model)
-    _test_scores(model, x_train, y_train, x_test, y_test)
-    _test_metrics(metrics)
+    _verificar_componentes(modelo_entrenado)
+    _probar_scores(modelo_entrenado, x_train, y_train, x_test, y_test)
+    _comprobar_metricas(metricas_generadas)
